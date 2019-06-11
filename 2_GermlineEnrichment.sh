12b5f8fc2e5aaf9da98c2f623432d223b154e13d
josephhalstead <josephhalstead89@gmail.com>
2019-06-11 15:50:56 +0100

    draft revision for the addition of decon

--- 2_GermlineEnrichment.sh
+++ 2_GermlineEnrichment.sh
@@ -267,59 +267,27 @@ awk '$1 !~ /^MT/ { print $0 }' "$seqId"_filtered_annotated_roi.vcf > "$seqId"_fi
 
 ### CNV analysis ###
 
-#check one or more samples have high coverage
-if [[ -e "HighCoverageBams.list" ]] && [[ $(wc -l "HighCoverageBams.list" | awk '{print $1}') -gt 4 ]]; then
+#check one or more samples have high coverage and then call CNVs
+if [[ -e "HighCoverageBams.list" ]] && [[ $panel == "IlluminaTruSightCancer" ]] && [[ $(wc -l "HighCoverageBams.list" | awk '{print $1}') -gt 4 ]]; then
 
-    #make CNV bed
-    /share/apps/bedtools-distros/bedtools-2.26.0/bin/bedtools slop \
-    -i /data/diagnostics/pipelines/GermlineEnrichment/GermlineEnrichment-"$version"/"$panel"/"$panel"_ROI_b37.bed \
-    -g /data/db/human/gatk/2.8/b37/human_g1k_v37.fasta.fai \
-    -b 250 | \
-    grep -v ^X| \
-    grep -v ^Y | \
-    grep -v ^MT | \
-    /share/apps/bedtools-distros/bedtools-2.26.0/bin/bedtools sort \
-    -faidx /data/db/human/gatk/2.8/b37/human_g1k_v37.fasta.fai | \
-    /share/apps/bedtools-distros/bedtools-2.26.0/bin/bedtools merge | \
-    /share/apps/bedtools-distros/bedtools-2.26.0/bin/bedtools subtract \
-    -A -a - -b /data/db/human/genomicSuperDups/genomicSuperDups_b37.bed.gz | \
-    awk -F"\t" '{print $1"\t"$2"\t"$3"\tr"NR}' > "$panel"_ROI_b37_CNV.bed
+    source /home/transfer/miniconda3/bin/activate germline_cnv
 
-    #call CNVs using read depth
-    /share/apps/R-distros/R-3.3.1/bin/Rscript /data/diagnostics/pipelines/GermlineEnrichment/GermlineEnrichment-"$version"/ExomeDepth.R \
-    -b HighCoverageBams.list \
-    -f /state/partition1/db/human/gatk/2.8/b37/human_g1k_v37.fasta \
-    -r "$panel"_ROI_b37_CNV.bed \
-    2>&1 | tee ExomeDepth.log
+    # run_decon script to actually call the cnvs
+    bash /data/diagnostics/pipelines/GermlineEnrichment/GermlineEnrichment-"$version"/sv_calling/run_decon.sh \
+        /data/diagnostics/pipelines/GermlineEnrichment/GermlineEnrichment-"$version"/"$panel"/"$panel"_ROI_b37_CNV.bed \
+        HighCoverageBams.list \
+        /state/partition1/db/human/gatk/2.8/b37/human_g1k_v37.fasta \
+        $seqId \
+        $version
 
-    #print ExomeDepth metrics
-    echo -e "BamPath\tFragments\tCorrelation" > "$seqId"_ExomeDepth_Metrics.txt
-    paste HighCoverageBams.list \
-    <(grep "Number of counted fragments" ExomeDepth.log | cut -d' ' -f6) \
-    <(grep "Correlation between reference and tests count" ExomeDepth.log | cut -d' ' -f8) >> "$seqId"_ExomeDepth_Metrics.txt
+    # combine the decon and manta calls into a single file
+    python /data/diagnostics/pipelines/GermlineEnrichment/GermlineEnrichment-"$version"/sv_calling/create_cnv_report.py \
+        --gene_bed_file /data/diagnostics/pipelines/GermlineEnrichment/GermlineEnrichment-"$version"/"$panel"/"$panel"_cnv.bed \
+        --bam_list HighCoverageBams.list \
+        --worksheet_id $seqId \
+        --output sv_analysis/"$seqId"_cnvReport.csv
 
-    #add CNV vcf headers and move to sample folder
-    for vcf in $(ls *_cnv.vcf); do
-
-        prefix=$(echo "$vcf" | sed 's/\.vcf//g')
-        sampleId=$(/share/apps/bcftools-distros/bcftools-1.4.1/bcftools query -l "$vcf")
-
-        #add VCF headers
-        /share/apps/jre-distros/jre1.8.0_131/bin/java -XX:GCTimeLimit=50 -XX:GCHeapFreeLimit=10 -Djava.io.tmpdir=/state/partition1/tmpdir -Xmx2g -jar /share/apps/picard-tools-distros/picard-tools-2.12.2/picard.jar UpdateVcfSequenceDictionary \
-        I="$vcf" \
-        O="$sampleId"/"$seqId"_"$sampleId"_cnv.vcf \
-        SD=/state/partition1/db/human/gatk/2.8/b37/human_g1k_v37.dict
-
-        #gzip and tabix
-        /share/apps/htslib-distros/htslib-1.4.1/bgzip "$sampleId"/"$seqId"_"$sampleId"_cnv.vcf
-        /share/apps/htslib-distros/htslib-1.4.1/tabix -p vcf "$sampleId"/"$seqId"_"$sampleId"_cnv.vcf.gz
-
-    done
-
-    #move cnv metrics to sample folder
-    for i in $(ls *cnv.txt); do
-        mv "$i" $(echo "$i" | cut -d_ -f1)/"$seqId"_"$i";
-    done
+    source /home/transfer/miniconda3/bin/deactivate
 
 fi
 
@@ -329,10 +297,6 @@ then
     # generate single bedfile from gene beds in order to enable custom reporting of gaps and coverage for TSC panel
     echo "making hotspots bedfile"
     cat /data/diagnostics/pipelines/GermlineEnrichment/GermlineEnrichment-"$version"/IlluminaTruSightCancer/hotspot_coverage/*.bed | sort -k1,1 -k2,2n > /data/results/$seqId/$panel/IlluminaTruSightCancer_CustomROI_b37.bed
-
-    # generate combined CNV report
-    echo "making CNV Report"
-    /share/apps/anaconda2/bin/Rscript --vanilla /data/diagnostics/pipelines/GermlineEnrichment/GermlineEnrichment-"$version"/generateCnvReport.R $seqId $panel $version
 
     # create a gaps & coverage files that are panel specific
     echo "calculating custom coverage"
@@ -369,4 +333,4 @@ rm "$seqId"_variants_filtered_genotypes_filtered_meta.vcf "$seqId"_variants_filt
 rm "$seqId"_variants_filtered_genotypes_filtered_meta_vep.vcf.idx "$seqId"_variants_filtered_genotypes_filtered.vcf
 rm "$seqId"_variants_filtered_genotypes_filtered.vcf.idx
 rm "$seqId"_variants_filtered.vcf "$seqId"_variants_filtered.vcf.idx
-rm -f ExomeDepth.log HighCoverageBams.list "$seqId"_*_cnv.vcf
+rm -f ExomeDepth.log "$seqId"_*_cnv.vcf
---
+++ sv_calling/create_cnv_report.py
@@ -0,0 +1,327 @@
+import pandas as pd
+import glob
+from pysam import VariantFile
+import csv
+import argparse
+
+# Get arguments
+parser = argparse.ArgumentParser(
+	formatter_class=argparse.RawTextHelpFormatter,
+	description='Program to merge SV calls from Manta and Decon.')
+
+parser.add_argument('--gene_bed_file', type=str, nargs=1, required=True,
+					help='The bed file of regions with their associated gene annotations.')
+
+parser.add_argument('--bam_list', type=str, nargs=1, required=True,
+					help='A text file containing the high coverage bams used by Manta and Decon.')
+
+parser.add_argument('--worksheet_id', type=str, nargs=1, required=True,
+					help='The worksheet id.')
+
+parser.add_argument('--output', type=str, nargs=1, required=True,
+					help='Name of the output CSV.')
+
+args = parser.parse_args()
+
+# config variables
+
+gene_bed_file = config = args.gene_bed_file[0]
+bam_list = args.bam_list[0]
+worksheet_id = args.worksheet_id[0]
+output = args.output[0]
+
+decon_csv = f'sv_analysis/{worksheet_id}_all.txt'
+manta_path = '*/*_sv_filtered.vcf.gz'
+sample_depths = '*/*_DepthOfCoverage.sample_summary'
+
+
+# Useful Functions
+
+def create_manta_df(manta_vcfs):
+	"""
+	Loop through manta VCFs and create a dataframe.
+
+	"""
+	
+	manta_list = []
+	
+	for vcf in manta_vcfs:
+		
+		sample_name = vcf.split('/')[-2]
+		
+		# ignore vcfs made from non high coverage samples.
+		if sample_name in high_cov_sample_list:
+		
+			vcf_in = VariantFile(vcf)
+
+			for rec in vcf_in.fetch():
+
+				chrom = rec.chrom
+				start = rec.pos
+				end = rec.stop
+				ref = rec.ref
+				alt = rec.alts[0]
+				sv_type = rec.info['SVTYPE']
+				sv_filter = ';'.join(rec.filter.keys())
+
+				manta_list.append([sample_name, 'manta', chrom, start, end, ref, alt, sv_type, '-', sv_filter, 'NA'])
+			
+			
+	manta_df = pd.DataFrame(manta_list, columns=['sample_name',
+												 'caller',
+												 'chromosome',
+												 'start',
+												 'end',
+												 'ref',
+												 'alt',
+												 'variant_type',
+												 'n_affected_exons',
+												 'variant_filter',
+												 'decon_correlation'])
+	
+	return manta_df
+
+
+def get_overlapping_genes(df, grouped_bed):
+	"""
+	Get the custom roi file and check to see if any of our SVs overlap with the regions.
+	"""
+
+	sv_start = df['start']
+
+	if sv_start == 'NA':
+
+		return 'NA'
+	
+	if pd.isna(df['gene']) == False:
+		
+		return df['gene']
+
+	sv_chrom = str(df['chromosome'])
+	sv_start = int(df['start'])
+	sv_end = int(df['end'])
+
+	gene_list = []
+
+	for gene in grouped_bed.itertuples():
+
+		gene_chrom = str(gene.chrom)
+		gene_start = int(gene.start)
+		gene_end = int(gene.end)
+
+		# variant start is within the gene
+		if gene_chrom == sv_chrom and gene_start <= sv_start <= gene_end:
+
+			gene_list.append(gene.gene)
+
+		# variant end is within the gene
+		elif gene_chrom == sv_chrom and gene_start <= sv_end <= gene_end:
+
+			gene_list.append(gene.gene)
+
+		# variant start is before gene and variant end is after gene e.g. variant engulfs gene.
+		elif gene_chrom == sv_chrom and (sv_start <= gene_start and sv_end >= gene_end):
+
+			gene_list.append(gene.gene)
+
+	if len(gene_list) > 0:
+
+		return '|'.join(list(set(gene_list)))
+
+	else:
+
+		return 'NA'
+
+def add_sample_filter(df):
+	"""
+	Add a sample level filter for low coverage samples and \
+	samples with low r2 score from Decon.
+
+	"""
+	
+	sample_filter = []
+	
+	if df['decon_correlation'] != 'NA':
+		
+		if df['decon_correlation'] < 0.98:
+			
+			sample_filter.append('R2<0.98')
+		
+	if df['mean'] != 'NA':
+				
+		if df['mean'] < 160:
+					
+			sample_filter.append('Depth<160')
+	
+	if len(sample_filter) ==0:
+		
+		return 'PASS'
+	
+	else:
+		
+		return ';'.join(sample_filter)       
+
+def fix_variant_type(df):
+	"""
+	Unify the variant type field between Manta and Decon.
+
+	"""
+	if df['variant_type'] == 'BND':
+		
+		return 'breakend'
+	
+	elif df['variant_type'] == 'INS':
+		
+		return 'insertion'
+	
+	elif df['variant_type'] == 'DUP':
+		
+		return 'duplication'
+	
+	elif df['variant_type'] == 'DEL':
+		
+		return 'deletion'
+	
+	elif df['variant_type'] == 'INV':
+		
+		return 'inversion'
+	
+	else:
+		
+		return df['variant_type']
+
+
+# Open high coverage bam list and create list of high coverage bam files.
+high_cov_sample_list = []
+
+with open(bam_list, newline='') as csvfile:
+	spamreader = csv.reader(csvfile, delimiter=' ')
+	for row in spamreader:
+		high_cov_sample_list.append(row[0].split('/')[0])
+
+# Open decon CSV as dataframe
+decon_df = pd.read_csv(decon_csv, sep='\t')
+
+# Change column names and add other columns that we need
+decon_df['correlation'] = decon_df['Correlation']
+decon_df['chromosome'] = decon_df['Chromosome']
+decon_df['start'] = decon_df['Start']
+decon_df['end'] = decon_df['End']
+decon_df['variant_type'] = decon_df['CNV.type']
+decon_df['variant_filter'] = 'PASS'
+decon_df['exons_affected'] = decon_df['N.exons']
+decon_df['gene'] = decon_df['Gene']
+decon_df['caller'] = 'decon'
+decon_df['sample_name'] = decon_df['Sample'].apply(lambda x: x.split('_')[-1])
+decon_df['decon_correlation'] = decon_df['Correlation']
+decon_df['n_affected_exons'] = decon_df['N.exons']
+decon_df['ref'] = 'NA'
+decon_df['alt'] = 'NA'
+
+# Get the list of manta vcfs to open and then create Manta df
+manta_vcfs = glob.glob(manta_path)
+manta_df = create_manta_df(manta_vcfs)
+
+# Get list of sample depth summary files to open
+sample_depths_files = glob.glob(sample_depths)
+
+# Merge the manta and decon dfs
+merged_df = manta_df.append(decon_df, sort=False)[['sample_name',
+ 'caller',
+ 'chromosome',
+ 'start',
+ 'end',
+ 'ref',
+ 'variant_type',
+ 'n_affected_exons',
+ 'variant_filter',
+ 'gene',
+ 'decon_correlation' ]]
+
+
+# we need to add a row with NAs if a sample has not had any calls for that caller
+# For example if no manta calls add row like sample_name,manta,NA,NA etc.
+grouped = merged_df.groupby(['sample_name', 'caller']).mean().reset_index()
+
+decon_calls = {}
+manta_calls = {}
+
+# create dictionaries of samples with calls
+for row in grouped.itertuples():
+    
+    if row.caller == 'decon':
+        
+        decon_calls[row.sample_name] = True
+        
+    if row.caller == 'manta':
+        
+        manta_calls[row.sample_name] = True
+
+
+additional_na = []
+
+# Add a NA row for samples without calls for that caller
+for sample in high_cov_sample_list:
+    
+    if sample not in manta_calls:
+        
+        additional_na.append([sample, 'manta', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA'])
+        
+    if sample not in decon_calls:
+        
+        additional_na.append([sample, 'decon', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA'])
+        
+additional_df = pd.DataFrame(additional_na, columns=['sample_name',
+                                                 'caller',
+                                                 'chromosome',
+                                                 'start',
+                                                 'end',
+                                                 'ref',
+                                                 'alt',
+                                                 'variant_type',
+                                                 'n_affected_exons',
+                                                 'variant_filter',
+                                                 'decon_correlation'])
+
+# Merge merged df and additional df
+final_df = merged_df.append(additional_df, sort=False).sort_values('sample_name')
+
+# Read in gene df
+gene_df = pd.read_csv(gene_bed_file, sep='\t', names=['chrom', 'start', 'end', 'gene'])
+
+# Collapse genes df into df with one gene in each row
+grouped_gene_df = gene_df.groupby(['chrom', 'gene']).agg({'start': 'min', 'end': 'max'})
+grouped_gene_df = grouped_gene_df.reset_index()
+
+# Annotate with gene information
+final_df['gene'] = final_df.apply(get_overlapping_genes, axis=1, args=(grouped_gene_df,))
+
+# Add depth information to final df
+depth_df = pd.DataFrame()
+
+for depth_file in sample_depths_files:
+    
+    new_depth_df = pd.read_csv(depth_file, sep='\t')
+    depth_df= depth_df.append(new_depth_df)
+
+final_df = final_df.merge(depth_df, how='left', left_on='sample_name', right_on='sample_id')
+
+final_df['sample_depth'] = final_df['mean']
+
+final_df['sample_filter'] = final_df.apply(add_sample_filter, axis=1)
+
+# unify variant type column
+final_df['variant_type'] = final_df.apply(fix_variant_type,axis=1)
+
+final_df[['sample_name',
+ 'caller',
+ 'chromosome',
+ 'start',
+ 'end',
+ 'ref',
+ 'variant_type',
+ 'n_affected_exons',
+ 'variant_filter',
+ 'sample_filter',
+ 'sample_depth',
+ 'gene']].to_csv(output, index=False, sep=',')
